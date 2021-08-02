371b42bd68f1017554773c73f09f79d2c62e059e
juandelia03 <juanchif5017@gmail.com>
2021-08-02 15:11:08 -0300

    commit

--- store.js
+++ store.js
@@ -3,10 +3,10 @@ import { reactive } from "vue";
 const state = reactive({
   //este valor tiene q ser false para que el usuario se tenga que loggear para usar la app
   //está en true para desarrollo mas comodo
-  logged: true,
+  logged: false,
   //este valor tiene q ser "" para acceder al usuario que corresponda
   //está en juan para no tener q logearme siempre
-  currentUser: "juan",
+  currentUser: "",
 });
 
 export default {
